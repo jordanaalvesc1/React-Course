e957d1c42aae27aecae5d9c94cf4bcc5e3fdd673
Jordana Alves Carneiro <jordanaalvescarneirou@gmail.com>
2024-02-28 11:06:22 -0300

    Creating components in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HellWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <h1>Olá React!</h1>
       <p>Meu primeiro App</p>
+      <HellWorld/>
     </div>
   );
 }
