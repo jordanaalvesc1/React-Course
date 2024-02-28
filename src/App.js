99d39bb1be7b05242b3fc8eef039fbce7fa81fd8
Jordana Alves Carneiro <jordanaalvescarneirou@gmail.com>
2024-02-28 13:53:48 -0300

    Inserting CSS into React

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import HellWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase'
 
 function App() {
   const nome = 'Maria'
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
       <HellWorld/>
       <SayMyName nome="Jordana"/>
       <SayMyName nome="Lucas"/>
