f96878f75c414f45e6be57f0806ca1631b4a43ba
Jordana Alves Carneiro <jordanaalvescarneirou@gmail.com>
2024-02-28 14:18:03 -0300

    Using React Fragments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import HellWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa';
+import Pessoa from './components/Pessoa'
 import Frase from './components/Frase'
+import List from './components/List';
 
 function App() {
   const nome = 'Maria'
@@ -17,6 +18,7 @@ function App() {
       <SayMyName nome={nome}/>
       <Pessoa nome= "Rodrigo" idade="28" profissao= "programador" foto="https:/via.placeholder.com/150"
       />
+      <List />
     </div>
   )
 }
