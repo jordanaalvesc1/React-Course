8f946bc787d24addd4aaf20ccd6b91a2d26a33d2
Jordana Alves Carneiro <jordanaalvescarneirou@gmail.com>
2024-02-28 13:16:55 -0300

    Working with Props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import HellWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa';
 
 function App() {
+  const nome = 'Maria'
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro App</p>
       <HellWorld/>
+      <SayMyName nome="Jordana"/>
+      <SayMyName nome="Lucas"/>
+      <SayMyName nome={nome}/>
+      <Pessoa nome= "Rodrigo" idade="28" profissao= "programador" foto="https:/via.placeholder.com/150"
+      />
     </div>
-  );
+  )
 }
 
 export default App;
