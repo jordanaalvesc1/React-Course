fa6b01032f653a44efe1e6430b4ea9833482e0d3
Jordana Alves Carneiro <jordanaalvescarneirou@gmail.com>
2024-02-29 10:21:29 -0300

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import Evento from './components/Evento';
-import Form from './components/Form';
+import Condicional from './components/Condicional';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento/>
-      <Form />
-    </div>
+      <h1>Renderização Condicional</h1>
+      <Condicional />
+        </div>
   )
 }
 
